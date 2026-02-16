007b4c5e7bf29ab4f10c29400cf64384641e775d
eeh10 <eeh10@kent.ac.uk>
2026-02-16 00:06:40 +0000

    layout

---
+++ fleet_manager.py
@@ -0,0 +1,5 @@
+def init_database()
+names = "Jean-Luc Picard", "William Riker", "Data", "Worf", "Beverly Crusher"]
+ranks = ["Captain", "Commander", "Lt. Commander", "Lieutenant", "Commander"]
+divs = ["Command", "Command", "Operations", "Security", "Sciences"]
+ids = [1001, 1002, 1003, 1004, 1005]
